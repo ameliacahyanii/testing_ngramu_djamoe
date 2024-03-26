a09603387bd0fecc9a1c6452240714d0815fe719
amelizyn <ameliaa.cahyaniii@gmail.com>
2024-03-26 08:13:18 +0700

    Adding Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,55 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './src/**/*/*.[png,svg]'
+        './src/**/*/*.[png,svg]',
+        '*.{html,js}',
+
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-                linux: ['Linux'],
-            },
-
-            backgroundImage: theme => ({
-                'logo': "url('./assets/img/logo.png')",
-            }),
-
-            colors: {
-                'primary': '#9E896A',
-                'secondary': '#FFF3DE',
-                'secondary-text': '#AFA08B',
-                'text-option': '#A88632',
-                'slider-btn': '#D1BB84',
+        container: {
+            padding: {
+                default: '15px',
             }
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '960px',
+            xl: '1200px',
+        },
+        fontFamily: {
+            sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            playfair: ['Playfair'],
+            // linux: ['Linux'],
+        },
+
+        backgroundImage: {
+            'logo': 'url(./assets/img/logo.png)',
+            'hero': 'url(./assets/img/product-1.png)',
+
+        },
+        extend: {
+            colors: {
+                primary: {
+                    default: '#9E896A',
+                    hover: '#BEB4A4',
+                },
+                secondary: '#FFF3DE',
+                accent: {
+                    default: '#D1BB84',
+                    secondary: '#A88632',
+                    hover: '#C3B288'
+                },
+                dark: '#35281E',
+            },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 };
